311d21b09d4cb07c4a3f631d068bf872e3521f27
sonu4sam <sonu4sam@outlook.com>
2023-07-24 23:58:53 -0500

    bugfix and database addition

--- reviews/views.py
+++ reviews/views.py
@@ -3,9 +3,8 @@ import requests
 
 
 def index(request):
-    
     return render(request, "base.html")
 
+
 def search_result(request, book_to_search):
-    
     return render(request, 'search.html', {'searched_book': book_to_search})
