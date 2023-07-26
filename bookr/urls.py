fae40abea6d6aaec77745fd339f64df7c17ca38c
sonu4sam <sonu4sam@outlook.com>
2023-07-26 00:39:39 -0500

    let's fix

--- bookr/urls.py
+++ bookr/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from reviews import admin
 from django.urls import include, path
-from reviews import views
+import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', reviews.views.index),
+   # path('book-search/', reviews.views.book_search, name='book_search'),
     path('', include('reviews.urls')),
 
 ]
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
+
 admin.site.register(Publisher)
 admin.site.register(Contributor)
 admin.site.register(Book)
 admin.site.register(BookContributor)
 admin.site.register(Review)
-
