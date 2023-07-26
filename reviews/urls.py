43a9284692a47a830c66df1eed21248c56ba5119
sonu4sam <sonu4sam@outlook.com>
2023-07-25 20:31:07 -0500

    bugfix and html

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book, Review
 from .utils import average_rating
 
 
 def book_list(request):
     books = Book.objects.all()
-    book_list = []
+    book_with_reviews = []
     for book in books:
-        reviews = book.review_set.all()
+        reviews = Review.objects.filter(book=book)
         if reviews:
             book_rating = average_rating([review.rating for review in reviews])
             num_of_reviews = len(reviews)
         else:
             book_rating = 0
             num_of_reviews = 0
-        book_list.append({'book': book, 'rating': book_rating, 'num_of_reviews': num_of_reviews})
-    context = {'book_list': book_list}
+        book_with_reviews.append({'book': book, 'rating': book_rating, 'reviews': reviews,
+                                  'num_of_reviews': num_of_reviews})
+    context = {'book_list': book_with_reviews}
     return render(request, 'reviews/books_list.html', context)
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {
+            'book': book,
+            'book_rating': book_rating,
+            'reviews': reviews,
+        }
+    else:
+        context = {
+            'book': book,
+            'book_rating': None,
+            'reviews': None,
+        }
+    return render(request, 'reviews/book_detail.html', context)
